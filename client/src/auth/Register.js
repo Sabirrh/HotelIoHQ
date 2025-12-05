e709fd3a55cd1edc390a41ab9734f7b4f9438067
Sabir Hussain Teli <101512108+Sabirrh@users.noreply.github.com>
2025-12-05 16:37:43 +0530

    Missing import react-router-dom

--- client/src/auth/Register.js
+++ client/src/auth/Register.js
@@ -1,6 +1,6 @@
 // src/auth/Register.js
 import { useState } from "react";
-import { useNavigate } from "react-router";
+import { useNavigate } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import { register } from "../actions/auth";
